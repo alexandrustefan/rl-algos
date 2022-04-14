4e9861acb4c8b0d4d28384dc0a110fd1e0d64825
Alexandru Stefan <stefan.i.alexandru@gmail.ccom>
2022-04-15 00:55:25 +0300

    multiple bug fixes in simple env

--- simple_v1_env.py
+++ simple_v1_env.py
@@ -10,7 +10,7 @@ MAX_NET_WORTH = 2147483647
 MAX_NUM_QUOTE_OR_BASE_ASSET = 2147483647
 
 INITIAL_QUOTE_ASSET = 10000
-INITIAL_BASE_ASSET = 10000
+INITIAL_BASE_ASSET = 0
 OBSERVATION_WINDOW_SIZE = 24 # Probably we should put it as param ?
 
 class SimpleTradingEnv(gym.Env):
@@ -83,6 +83,8 @@ class SimpleTradingEnv(gym.Env):
             # Buy only the amount that agent chose
             assets_bought = int(available_amount_to_buy_with * amount)
             # Update the quote asset
+            self._quote_asset -= assets_bought * current_price
+            # Update the base asset
             self._base_asset += assets_bought
             # substract trading fee from base asset based on the amount bought
             self._base_asset -= self._trading_fee * assets_bought
@@ -91,9 +93,12 @@ class SimpleTradingEnv(gym.Env):
         elif action_type < 2:
             # Sell % assets
             # Determine the amount of base asset that can be sold
-            amount_to_sell = int(self._quote_asset * amount)
+            amount_to_sell = int(self._base_asset * amount)
             received_quote_asset = amount_to_sell * current_price
+            # Update the quote asset
             self._quote_asset += received_quote_asset
+            # Update the base asset
+            self._base_asset -= amount_to_sell
             
             # substract trading fee from quote asset based on the amount sold
             self._quote_asset -= self._trading_fee * received_quote_asset
@@ -159,35 +164,65 @@ class SimpleTradingEnv(gym.Env):
         """
         Renders a plot with trades made by the agent.
         """
-        action_types, action_amounts, _ = zip(*self._action_history)
+        # action_types, action_amounts, _ = zip(*self._action_history)
 
-        def _plot_actions(action, candle):
-            color = None
-            if action == 'Sell':
-                color = 'red'
-            elif action == 'Buy':
-                color = 'green'
-            if color:
-                plt.scatter(candle, self.prices[candle], color=color)
+        # def _plot_actions(action, candle):
+        #     color = None
+        #     if action == 'Sell':
+        #         color = 'red'
+        #     elif action == 'Buy':
+        #         color = 'green'
+        #     if color:
+        #         plt.scatter(candle, self.prices[candle], color=color)
 
-        if self._first_rendering:
-            self._first_rendering = False
-            plt.cla()
-            # Plot the prices
-            plt.plot(self.prices)
+        # if self._first_rendering:
+        #     self._first_rendering = False
+        #     plt.cla()
+        #     # Plot the prices
+        #     plt.plot(self.prices)
             
-            start_action = action_types[0]
+        #     start_action = action_types[0]
 
-            _plot_actions(start_action, self._start_candle)
+        #     _plot_actions(start_action, self._start_candle)
 
-        _plot_actions(self._last_action_type, self._current_candle)
+        # _plot_actions(self._last_action_type, self._current_candle)
+
+        # plt.suptitle(
+        #     "Accumulated Reward: %.6f" % self._total_reward_accumulated + ' ~ ' +
+        #     "Net Worth: %.6f" % self._net_worth +
+        #     "Quote asset:  " + str(self._quote_asset) +
+        #     "Base asset:  " + str(self._base_asset)
+        # )
+
+        # plt.pause(0.01)
+        
+        
+        print(f'Accumulated Reward: {self._total_reward_accumulated} ---- Net Worth: {self._net_worth}')
+        print(f'Quote asset: {self._quote_asset} ---- Base asset: {self._base_asset}')
+        print(f'Last Action: {self._last_action_type} in percent of assets: {self._action_history[-1][1]} at price: {self._action_history[-1][2]}')
+        print(f'--------------------------------------------------------------------------------------')
+
+    def render_all(self, mode='human'):
+        window_ticks = np.arange(len(self._action_history))
+        plt.plot(self.prices)
+
+        short_ticks = []
+        long_ticks = []
+        for i, tick in enumerate(window_ticks):
+            if self._action_history[i] == 'Sell':
+                short_ticks.append(tick)
+            elif self._action_history[i] == 'Buy':
+                long_ticks.append(tick)
+
+        plt.plot(short_ticks, self.prices[short_ticks], 'ro')
+        plt.plot(long_ticks, self.prices[long_ticks], 'go')
 
         plt.suptitle(
-            "Accumulated Reward: %.6f" % self._total_reward_accumulated + ' ~ ' +
-            "Net Worth: %.6f" % self._net_worth
-        )
-
-        plt.pause(0.01)
+            "Total Reward: %.6f" % self._total_reward_accumulated + ' ~ ' +
+            "Total Profit: %.6f" % self._net_worth + ' ~~~~ ' +
+            "Quote asset:  " + str(self._quote_asset) + ' ~ ' +
+            "Base asset:  " + str(self._base_asset)
+        )            
 
     def _process_data(self, df):
         """
@@ -208,10 +243,17 @@ class SimpleTradingEnv(gym.Env):
 
         action_type_name = self._get_human_readable_action(action)
 
-        action_type = action_type_name
         amount = action[1]
-        return (action_type, amount, price)
+        return (action_type_name, amount, price)
 
     def _get_human_readable_action(self, action):
-        action_type_name = 'Buy' if action[0] < 1 else 'Sell' if action[0] < 2 else 'Hold'
+        # if action is lower than 1 than it's buy action, if action is lower than 2 than it's sell action otherwise it's hold action
+        action_type_name = None
+        if action[0] < 1:
+            action_type_name = 'Buy'
+        elif action[0] < 2:
+            action_type_name = 'Sell'
+        else:
+            action_type_name = 'Hold'
+        
         return action_type_name    
