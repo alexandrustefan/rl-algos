39f75da45555698d1e1a9cf666c955425d3fbeb9
Alexandru Stefan <stefan.i.alexandru@gmail.ccom>
2022-04-19 02:09:05 +0300

    vectorized the environment, some slight adjustments to the environment; added callbacks for saving models

--- simple_v1_env.py
+++ simple_v1_env.py
@@ -15,20 +15,22 @@ OBSERVATION_WINDOW_SIZE = 24 # Probably we should put it as param ?
 
 class SimpleTradingEnv(gym.Env):
     
-    metadata = {'render.modes': ['human']}
+    metadata = {'render.modes': ['live', 'human', 'none']}
+    visualization = None
 
-    def __init__(self, df, trading_fee):
+    def __init__(self, df_scaled, df_normal, trading_fee):
         
-        self.df = df
+        self.df_scaled = df_scaled.reset_index(drop=True)
+        self.df_normal = df_normal.reset_index(drop=True)
         self.window_size = OBSERVATION_WINDOW_SIZE
-        self.prices, self.features = self._process_data(df)
+        self.prices, self.features = self._process_data(df_scaled)
         # The shape of the observation is (window_size * features + environment_features) the environment_features are: quote_asset, base_asset, net_worth. The entire observation is flattened in a 1D np array. 
         self.obs_shape = ((OBSERVATION_WINDOW_SIZE * self.features.shape[1] + 3),)
 
-        print(self.obs_shape)
 
         # Action space
-        self.action_space = spaces.Box(low=np.array([0, 0]), high=np.array([3.0, 1.0]), dtype=np.float32)
+        #self.action_space = spaces.Box(low=np.array([0, 0]), high=np.array([3.0, 1.0]), dtype=np.float32)
+        self.action_space = spaces.MultiDiscrete([3, 100])
         # Observation space
         self.observation_space = spaces.Box(low=-1, high = 1, shape=self.obs_shape, dtype=np.float32)
 
@@ -46,11 +48,9 @@ class SimpleTradingEnv(gym.Env):
         self._previous_net_worth = None
 
         # Render and analysis data
-        self._action_history = None
         self._total_reward_accumulated = None
-        self.trade_history = None # will be a dict
+        self.trade_history = None
         self._first_rendering = None
-        self._last_action_type = None
         
 
     def reset(self):
@@ -60,43 +60,44 @@ class SimpleTradingEnv(gym.Env):
         self._base_asset = INITIAL_BASE_ASSET 
         self._net_worth = INITIAL_QUOTE_ASSET # at the begining our net worth is the initial quote asset
         self._previous_net_worth = INITIAL_QUOTE_ASSET # at the begining our previous net worth is the initial quote asset
-        self._action_history = []
         self._total_reward_accumulated = 0.
         self._first_rendering = True
-        self._last_action_type = 'Buy'
-        self.trade_history = {}
+        self.trade_history = []
         return self._get_observation()
 
     def _take_action(self, action):
         self._done = False
-        self._current_candle += 1
         current_price = random.uniform(
-            self.df.loc[self._current_candle, "low"], self.df.loc[self._current_candle, "high"])
+            self.df_normal.loc[self._current_candle, "low"], self.df_normal.loc[self._current_candle, "high"])
 
-        if self._current_candle == self._end_candle:
-            self._done = True
-
+        print('action: ', action)
+        print('action type: ', action[0])
+        print('action amount: ', action[1])
         action_type = action[0]
-        amount = action[1]
-
+        amount = action[1] / 100
+        
         if action_type < 1:
             # Buy % assets
             # Determine the maximum amount of quote asset that can be bought
-            available_amount_to_buy_with = int(self._quote_asset / current_price)
+            available_amount_to_buy_with = self._quote_asset / current_price
             # Buy only the amount that agent chose
-            assets_bought = int(available_amount_to_buy_with * amount)
-            # Update the quote asset
+            assets_bought = available_amount_to_buy_with * amount
+            # Update the quote asset balance
             self._quote_asset -= assets_bought * current_price
             # Update the base asset
             self._base_asset += assets_bought
             # substract trading fee from base asset based on the amount bought
             self._base_asset -= self._trading_fee * assets_bought
+
+            # Add to trade history the amount bought if greater than 0
+            if assets_bought > 0:
+                self.trade_history.append({'step': self._current_candle, 'type': 'Buy', 'amount': assets_bought, 'price': current_price, 'total' : assets_bought * current_price, 'percent_amount': action[1]})
         
 
         elif action_type < 2:
             # Sell % assets
             # Determine the amount of base asset that can be sold
-            amount_to_sell = int(self._base_asset * amount)
+            amount_to_sell = self._base_asset * amount
             received_quote_asset = amount_to_sell * current_price
             # Update the quote asset
             self._quote_asset += received_quote_asset
@@ -106,12 +107,17 @@ class SimpleTradingEnv(gym.Env):
             # substract trading fee from quote asset based on the amount sold
             self._quote_asset -= self._trading_fee * received_quote_asset
 
+            # Add to trade history the amount sold if greater than 0
+            if amount_to_sell > 0:
+                self.trade_history.append({'step': self._current_candle, 'type': 'Sell', 'amount': amount_to_sell, 'price': current_price, 'total' : received_quote_asset, 'percent_amount': action[1]})
+
+        else:
+            # Hold
+            self.trade_history.append({'step': self._current_candle, 'type': 'Hold', 'amount': '0', 'price': current_price, 'total' : 0, 'percent_amount': action[1]})
+
+
         # Update the current net worth
         self._net_worth = self._base_asset * current_price + self._quote_asset
-
-        # Add action to action history array as tuple
-        self._action_history.append(self._generate_action_data_tuple(action, current_price))
-        self._last_action_type = self._get_human_readable_action(action)
 
 
 
@@ -119,7 +125,7 @@ class SimpleTradingEnv(gym.Env):
         """
         Returns the next observation, reward, done and info.
         """
-        self._current_candle += 1
+        
         self._take_action(action)
 
         # Calculate reward comparing the current net worth with the previous net worth
@@ -137,11 +143,14 @@ class SimpleTradingEnv(gym.Env):
             net_worth = self._net_worth,
             action_type = self._get_human_readable_action(action),
             action_amount = action[1],
+            current_step = self._current_candle
         )
 
-        self._update_trade_history(info)
+        self._current_candle += 1
 
-
+        self._done = self._net_worth <= 0 or self._current_candle >= len(
+            self.df_scaled.loc[:, 'open'].values)
+        
         return obs, reward, self._done, info
 
 
@@ -152,91 +161,46 @@ class SimpleTradingEnv(gym.Env):
         data_frame = self.features[(self._current_candle - self.window_size):self._current_candle]
         data_frame = data_frame.flatten()
 
-
+        # Scale all the values to be between -1 and 1
         obs = np.append(data_frame, np.array([self._net_worth / MAX_NET_WORTH , self._quote_asset / MAX_NUM_QUOTE_OR_BASE_ASSET, self._base_asset / MAX_NUM_QUOTE_OR_BASE_ASSET], dtype=np.float32))
 
         return obs
 
-    def _update_trade_history(self, info):
-        if not self.trade_history:
-            self.trade_history = {key: [] for key in info.keys()}
 
-        for key, value in info.items():
-            self.trade_history[key].append(value)
-
-
-    def render(self, mode='human', close=False):
+    def render(self, mode='human', **kwargs):
         """
         Renders a plot with trades made by the agent.
         """
-        # action_types, action_amounts, _ = zip(*self._action_history)
-
-        # def _plot_actions(action, candle):
-        #     color = None
-        #     if action == 'Sell':
-        #         color = 'red'
-        #     elif action == 'Buy':
-        #         color = 'green'
-        #     if color:
-        #         plt.scatter(candle, self.prices[candle], color=color)
-
-        # if self._first_rendering:
-        #     self._first_rendering = False
-        #     plt.cla()
-        #     # Plot the prices
-        #     plt.plot(self.prices)
-            
-        #     start_action = action_types[0]
-
-        #     _plot_actions(start_action, self._start_candle)
-
-        # _plot_actions(self._last_action_type, self._current_candle)
-
-        # plt.suptitle(
-        #     "Accumulated Reward: %.6f" % self._total_reward_accumulated + ' ~ ' +
-        #     "Net Worth: %.6f" % self._net_worth +
-        #     "Quote asset:  " + str(self._quote_asset) +
-        #     "Base asset:  " + str(self._base_asset)
-        # )
-
-        # plt.pause(0.01)
         
+        if mode == 'human':
+            print(f'Accumulated Reward: {self._total_reward_accumulated} ---- Current Net Worth: {self._net_worth}')
+            print(f'Current Quote asset: {self._quote_asset} ---- Current Base asset: {self._base_asset}')
+            print(f'Number of trades: {len(self.trade_history)}')
         
-        print(f'Accumulated Reward: {self._total_reward_accumulated} ---- Net Worth: {self._net_worth}')
-        print(f'Quote asset: {self._quote_asset} ---- Base asset: {self._base_asset}')
-        print(f'Last Action: {self._last_action_type} in percent of assets: {self._action_history[-1][1]} at price: {self._action_history[-1][2]}')
-        print(f'--------------------------------------------------------------------------------------')
+            if(len(self.trade_history) > 0):
+                print(f'Last Action: {self.trade_history[-1]["type"]} {self.trade_history[-1]["amount"]} assets ({self.trade_history[-1]["percent_amount"]} %) at price {self.trade_history[-1]["price"]}, total: {self.trade_history[-1]["total"]}')
+            print(f'--------------------------------------------------------------------------------------')
+        elif mode == 'live':
+            if self.visualization == None:
+                self.visualization = LiveTradingGraph(self.df_normal, kwargs.get('title', None))
 
-    def render_all(self, mode='human'):
-        window_ticks = np.arange(len(self._action_history))
-        plt.plot(self.prices)
+            if self._current_candle > OBSERVATION_WINDOW_SIZE:
+                self.visualization.render(self._current_candle, self._net_worth, self.trade_history, window_size=OBSERVATION_WINDOW_SIZE)
 
-        short_ticks = []
-        long_ticks = []
-        for i, tick in enumerate(window_ticks):
-            if self._action_history[i] == 'Sell':
-                short_ticks.append(tick)
-            elif self._action_history[i] == 'Buy':
-                long_ticks.append(tick)
+    def close(self):
+        if self.visualization != None:
+            self.visualization.close()
+            self.visualization = None
+         
 
-        plt.plot(short_ticks, self.prices[short_ticks], 'ro')
-        plt.plot(long_ticks, self.prices[long_ticks], 'go')
-
-        plt.suptitle(
-            "Total Reward: %.6f" % self._total_reward_accumulated + ' ~ ' +
-            "Total Profit: %.6f" % self._net_worth + ' ~~~~ ' +
-            "Quote asset:  " + str(self._quote_asset) + ' ~ ' +
-            "Base asset:  " + str(self._base_asset)
-        )            
-
-    def _process_data(self, df):
+    def _process_data(self, df_scaled):
         """
         Processes the dataframe into features.
         """
 
-        prices = self.df.loc[:, 'close'].to_numpy(dtype=np.float32)
+        prices = self.df_scaled.loc[:, 'close'].to_numpy(dtype=np.float32)
 
-        data_frame = df.iloc[:, 1:] # drop first column which is date TODO: Should be fixed outside of this class
+        data_frame = df_scaled.iloc[:, 1:] # drop first column which is date TODO: Should be fixed outside of this class
 
         # Convert df to numpy array
         return prices, data_frame.to_numpy(dtype=np.float32)
